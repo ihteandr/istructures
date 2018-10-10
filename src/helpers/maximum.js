972b8fdc7a0855bef28cde00ab2d88dfc157bd3a
ghukasyan.vladimir@gmail.com <8Dectember1988>
2018-10-10 23:50:20 +0400

    fix stupid issue

--- src/helpers/maximum.js
+++ src/helpers/maximum.js
@@ -3,9 +3,9 @@ export function maximum(arr, k) {
     if (arr.length < k) {
         return null;
     }
+    k = k || 1;
     const maximums = new Array(k);
     maximums.fill(Number.MIN_SAFE_INTEGER);
-    k = k || 1;
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < maximums.length; j++) {
             if (maximums[j] < arr[i]) {
--- src/helpers/minimum.js
+++ src/helpers/minimum.js
@@ -4,9 +4,9 @@ export function minimum(arr, k) {
     if (arr.length < k) {
         return null;
     }
+    k = k || 1;
     const minimums = new Array(k);
     minimums.fill(Number.MAX_SAFE_INTEGER);
-    k = k || 1;
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < minimums.length; j++) {
             if (minimums[j] > arr[i]) {
